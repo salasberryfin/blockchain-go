45643a87d868ef4e21d62c1760eb8928c23a117a
Carlos Salas <carlos.salas@keepler.io>
2019-12-17 00:32:22 +0100

    Implement node API to retrieve blockchain from neighbours

--- main.go
+++ main.go
@@ -4,53 +4,118 @@ import (
     "fmt"
     "log"
     "crypto/sha256"
-    "hash"
     "io/ioutil"
-    //"errors"
+    "time"
+    "strings"
     "net/http"
-    //"encoding/json"
+    "encoding/hex"
+    "encoding/json"
 )
 
+const node_api_port string = ":5000"
+const node_api_url string = "localhost" + node_api_port
 const main_api string = "http://localhost:8080/get_nodes"
 var Nodes = []string{}
 
 type Block struct {
-    Hash hash.Hash
-    PreviousHash hash.Hash
-    Transaction string
+    Timestamp       int64           `json:"Timestamp"`
+    Proof           string          `json:"Proof"`
+    PreviousHash    string          `json:"PreviousHash"`
+    Transaction     Transaction     `json:"Transaction"`
+}
+
+type Transaction struct {
+    Source      int
+    Recipient   int
+    Amount      string
 }
 
 var blockchain []Block;
 
-func getSha256 (unhashed string) hash.Hash {
+func getSha256 (unhashed string) string {
     unhashedBytes := []byte(unhashed)
     hashed := sha256.New()
     hashed.Write(unhashedBytes)
 
-    return hashed
-}
-
-func getPreviousBlock() Block {
-    block := Block{}
-
-    return block
+    return hex.EncodeToString(hashed.Sum(nil))
 }
 
 func validateBlock() bool {
 
-    return false
+    return true
 }
 
 func addToChain(block Block) {
+    blockchain = append(blockchain, block)
 }
 
-func generateBlock (hash hash.Hash, transaction string) Block {
+func getPreviousBlock() Block {
+    if len(blockchain) > 0 {
+        return blockchain[len(blockchain)-1]
+    }
+
+    return Block{}
+}
+
+func generateBlock (transaction Transaction) Block {
     block := Block{}
-    block.Hash = hash
+    timestamp := time.Now().Unix()
+    block.Proof = "something"
+    block.Timestamp = timestamp
     block.PreviousHash = getSha256(fmt.Sprintf("%v", getPreviousBlock()))
     block.Transaction = transaction
 
     return block
+}
+
+
+func checkForLongerChain() ([]string) {
+    nodesString, err := retrieveNodes()
+    Nodes = strings.Split(nodesString, ",")
+    if err != nil {
+        log.Fatal(err)
+    }
+    for i := 0; i < len(Nodes); i++ {
+        u := Nodes[i]
+        u = u[1:len(u)-1]
+        if u == node_api_url {
+            log.Print("Avoiding sending a request to ourselves.")
+            continue
+        }
+        log.Print("Sending request to: ", u)
+        r, err := http.Get("http://" + u + "/get_chain")
+        if err != nil {
+            log.Print("[Error]: ", err)
+            return []string{fmt.Sprintf("An error was encountered when calling %v", u)}
+        }
+        defer r.Body.Close()
+        responseData, err := ioutil.ReadAll(r.Body)
+        var newBlockchain []Block
+        json.Unmarshal(responseData, &newBlockchain)
+        fmt.Printf("Parsed JSON Response: %v", newBlockchain)
+        if len(blockchain) < len(newBlockchain) {
+            log.Print("Retrieved blockchain is longer.")
+            blockchain = newBlockchain
+        }
+    }
+
+    return []string{"Chain is now up to date - You can check the current status by calling /get_chain"}
+}
+
+func getCurrentChain() (string) {
+    var resp string
+    for i := 0; i < len(blockchain); i++ {
+        b, err := json.Marshal(blockchain[i])
+        if err != nil {
+            log.Fatal(err)
+        }
+        resp += string(b)
+        if i < len(blockchain) - 1 {
+            resp += ","
+        }
+    }
+
+    return resp
 }
 
 func retrieveNodes() (string, error) {
@@ -63,24 +128,19 @@ func retrieveNodes() (string, error) {
     if err != nil {
         log.Fatal(err)
     }
-    responseString := string(responseData)
-    log.Print(responseString)
+    responseString := strings.Trim(string(responseData), "[]")
 
     return responseString, nil
 }
 
 func main() {
-    //hash := getSha256("new block")
-    //log.Printf("%x", hash.Sum(nil))
-    //transaction := "transaction data"
-    //new_block := generateBlock(hash, transaction)
-    //log.Print("new block: ", new_block.Transaction)
-    log.Print("Starting API...")
-    Nodes, err := retrieveNodes()
-    if err != nil {
-        log.Fatal("Could not retrieve the current cluster.")
-    }
-    log.Print("Updated list of nodes: ", Nodes)
-    NodeApi()
+    transaction := Transaction{0, 0, "Genesis"}
+    new_block := generateBlock(transaction)
+    addToChain(new_block)
+    transaction2 := Transaction{1, 1, "Second"}
+    new_block2 := generateBlock(transaction2)
+    addToChain(new_block2)
+    log.Printf("Node API is running - port: %v", strings.Trim(node_api_port, ":"))
+    NodeApi(node_api_port)
 }
 
--- node-api.go
+++ node-api.go
@@ -2,22 +2,21 @@ package main
 
 import (
     "log"
+    "fmt"
     "net/http"
     "encoding/json"
 
     "github.com/gorilla/mux"
 )
 
-const port string = ":5000"
-
 func convertToJson(parsed []string) ([]byte, error) {
     return json.Marshal(parsed)
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
+func updateChain(w http.ResponseWriter, r *http.Request) {
+    resp := checkForLongerChain()
     w.Header().Set("Content-Type", "application/json")
     w.WriteHeader(http.StatusOK)
-    resp := []string{"You're running a valid node in port " + port}
     js, err := convertToJson(resp)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -25,12 +24,46 @@ func home(w http.ResponseWriter, r *http.Request) {
     w.Write(js)
 }
 
-func NodeApi() {
-    r := mux.NewRouter()
+func getChain(w http.ResponseWriter, r *http.Request) {
+    resp := getCurrentChain()
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    for i := 0; i < len(resp); i++ {
+    }
+    js := []byte(fmt.Sprintf("[%v]", resp))
+    w.Write(js)
+}
 
-    //r.HandleFunc("/new_transaction/", manageNode).Methods(http.MethodPost)
-    //r.HandleFunc("/get_nodes", getNodes).Methods(http.MethodGet)
+func newTransaction(w http.ResponseWriter, r *http.Request) {
+    /**
+    Generate new transaction:
+    - source
+    - destination
+    - amount
+    Identify nodes by port in which the API is running
+    **/
+    //source := strings.Split(port, ":")[1]
+    //params := mux.Vars(r)
+    //w.Header().Set("Content-Type", "application/json")
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    resp := []string{"You're running a valid node"}
+    js, err := convertToJson(resp)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+    w.Write(js)
+}
+
+func NodeApi(port string) {
+    r := mux.NewRouter()
     r.HandleFunc("/", home).Methods(http.MethodGet)
+    r.HandleFunc("/new_transaction/amount/{amount}/recipient/{target}", newTransaction).Methods(http.MethodPost)
+    r.HandleFunc("/get_chain", getChain).Methods(http.MethodGet)
+    r.HandleFunc("/update_chain", updateChain).Methods(http.MethodGet)
 
     err := http.ListenAndServe(port, r)
     if err != nil {
